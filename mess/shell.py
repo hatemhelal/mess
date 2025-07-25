Fix broadcasting when evaluating spherical transformation for contracted d-orbitals (#43)

mess/shell.py
@@ -109,7 +109,7 @@ def make_spherical_shell(L, center, alphas, coefficients):
         num_primitives = len(alphas)
 
         lmns = np.tile(np.array(lmns, dtype=np.int32), (num_primitives, 1))
-        sph_c = sph_c[:, None] * coefficients[None, :]
+        sph_c = coefficients[:, None] * sph_c[None, :]
         sph_alphas = np.repeat(alphas, num_cart_components)
 
         ao = make_contraction(center, sph_alphas, lmns, sph_c.reshape(-1))

test/test_interop.py
@@ -11,7 +11,7 @@ from conftest import is_mem_limited
 
 mol_cases = {
     "water": molecule("water"),
-    "Ar": Structure(np.asarray(18), np.zeros(3)),
+    "Kr": Structure(np.asarray(36), np.zeros(3)),
 }
 
 