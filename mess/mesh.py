79a0fa49ac1a1b7ac08407260b1f14ed528bb815
Hatem Helal <hatem.helal@gmail.com>
2025-08-04 21:42:22 +0100

    Adding standard grid implementation (sg1) (#47)
    * Adding standard grid implementation (sg1)
    
    * vectorise or die tryin
    
    * fixes
    
    * bump max points in plot mesh

--- mess/mesh.py
+++ mess/mesh.py
@@ -2,11 +2,14 @@
 
 from typing import Optional, Tuple, Union
 
+import numpy as np
 import equinox as eqx
 import jax.numpy as jnp
 from pyscf import dft
 from jax import vjp
+from scipy.integrate import lebedev_rule
 
+from mess.atomic_constants import sg1_atomic_radii
 from mess.basis import Basis
 from mess.interop import to_pyscf
 from mess.structure import Structure
@@ -74,3 +77,58 @@ def xcmesh_from_pyscf(structure: Structure, level: int = 3) -> Mesh:
     grids.level = level
     grids.build()
     return Mesh(points=grids.coords, weights=grids.weights)
+
+
+def cell_function(mu, k=3):
+    def f(x):
+        for _ in range(k):
+            x = 1.5 * x - 0.5 * x**3
+        return x
+
+    return 0.5 * (1 - f(mu))
+
+
+def sg1_mesh(
+    structure: Structure,
+    num_radial: int = 50,
+    angular_order: int = 23,
+    epsilon: float = 1e-12,
+) -> Mesh:
+    atom_radius = sg1_atomic_radii()[structure.atomic_number]
+    atom_radius = atom_radius.reshape(-1, 1)
+    ii = np.arange(1, num_radial + 1)
+    rad_weights = (
+        2 * atom_radius**3 * (num_radial + 1) * ii**5 / (num_radial + 1 - ii) ** 7
+    )
+    rad_points = atom_radius * ii**2 / (num_radial + 1 - ii) ** 2
+    ang_points, ang_weights = lebedev_rule(angular_order)
+
+    # Outer product of radial and angular points to form atom centered meshes
+    # [num_atoms, num_rad] x [num_ang, 3] -> [num_atoms, num_rad, num_ang, 3]
+    points = np.einsum("ij,kl->ijlk", rad_points, ang_points)
+    points = points + structure.position[:, None, None, :]
+
+    # [num_atoms, num_rad] x [num_ang -> [num_atoms, num_rad, num_ang]
+    weights = np.einsum("ij,l->ijl", rad_weights, ang_weights)
+
+    # Points and weights grouped by atom
+    points = points.reshape(structure.num_atoms, -1, 3)
+    weights = weights.reshape(structure.num_atoms, -1)
+
+    ii, jj = np.triu_indices(structure.num_atoms, 1)
+    ri_vec = points[:, None, :] - structure.position[None, ii, None, :]
+    ri = np.linalg.norm(ri_vec, axis=-1)
+
+    rj_vec = points[:, None, :] - structure.position[None, jj, None, :]
+    rj = np.linalg.norm(rj_vec, axis=-1)
+
+    R_ij_vec = structure.position[ii, :] - structure.position[jj, :]
+    R_ij = np.linalg.norm(R_ij_vec, axis=1)
+
+    mu = (ri - rj) / (R_ij[:, None] + epsilon)
+    s = cell_function(mu)
+    P = np.prod(s, axis=1)
+    w = P / (np.sum(P, axis=0) + epsilon)
+
+    weights = weights * w
+    return Mesh(points.reshape(-1, 3), weights.reshape(-1))
