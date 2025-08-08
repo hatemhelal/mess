b009a03314d743d8775260a2d9759f896fa8729b
Hatem Helal <hatem.helal@gmail.com>
2025-08-08 13:37:06 +0100

    add grad of charge wrt position test

--- test/test_mesh.py
+++ test/test_mesh.py
@@ -1,10 +1,13 @@
+import equinox as eqx
+import jax
+import numpy as np
 import pytest
 from numpy.testing import assert_allclose
 
-from mess.mesh import sg1_mesh, density
+from mess.basis import basisset
+from mess.mesh import density, sg1_mesh
 from mess.primitive import Primitive, product
 from mess.structure import Structure, molecule
-from mess.basis import basisset
 
 
 @pytest.mark.parametrize("lmn", [(0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1)])
@@ -27,3 +30,17 @@ def test_charge_quadrature():
     basis = basisset(structure, "sto-3g")
     actual_Q = mesh.weights @ density(basis, mesh)
     assert_allclose(actual_Q, structure.num_electrons)
+
+
+def test_charge_grad():
+    @jax.grad
+    def charge(pos, rest):
+        structure = eqx.combine(pos, rest)
+        mesh = sg1_mesh(structure)
+        return mesh.weights @ density(basis, mesh)
+
+    structure = molecule("benzene")
+    basis = basisset(structure, "sto-3g")
+    pos, rest = eqx.partition(structure, lambda x: id(x) == id(structure.position))
+    grad_q = charge(pos, rest)
+    assert_allclose(grad_q.position, np.zeros_like(structure.position), atol=1e-2)
