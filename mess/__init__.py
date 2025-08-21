87ef3a7425bd3ebb7fd95105793a481a065315ad
Hatem Helal <hatem.helal@gmail.com>
2025-08-21 14:56:39 +0100

    hot fix package for deploy stage

--- mess/__init__.py
+++ mess/__init__.py
@@ -11,7 +11,7 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("mess")
 except PackageNotFoundError:  # Package is not installed
-    __version__ = "dev"
+    __version__ = "0.0.0.dev0"
 
 from mess.basis import basisset
 from mess.hamiltonian import Hamiltonian, minimise
