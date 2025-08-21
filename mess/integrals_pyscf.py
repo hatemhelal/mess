daec8d59febb7df56a79d7e3a8fd170a77f233bd
Hatem Helal <hatem.helal@gmail.com>
2025-08-21 14:35:05 +0100

    wrap more integrals

--- mess/integrals_pyscf.py
+++ mess/integrals_pyscf.py
@@ -1,17 +1,42 @@
 import jax
 import jax.numpy as jnp
 from mess.interop import to_pyscf
-from mess.types import FloatNxN, default_fptype
+from mess.types import FloatNxN, FloatNxNxNxN, default_fptype
 from mess.basis import Basis
 
 
-def overlap_pyscf(basis: Basis) -> FloatNxN:
+def _1e_pyscf(basis: Basis, name: str) -> FloatNxN:
     def func(basis: Basis) -> FloatNxN:
         mol = to_pyscf(basis.structure, basis.basis_name)
         kind = "sph" if basis.spherical else "cart"
-        return jnp.array(mol.intor(f"int1e_ovlp_{kind}"), dtype=default_fptype())
+        return jnp.array(mol.intor(f"int1e_{name}_{kind}"), dtype=default_fptype())
 
     out_spec = jax.ShapeDtypeStruct(
         shape=2 * (basis.num_orbitals,), dtype=default_fptype()
     )
+    return jax.pure_callback(func, out_spec, basis, vmap_method="sequential")
+
+
+def overlap_pyscf(basis: Basis) -> FloatNxN:
+    return _1e_pyscf(basis, "ovlp")
+
+
+def kinetic_pyscf(basis: Basis) -> FloatNxN:
+    return _1e_pyscf(basis, "kin")
+
+
+def nuclear_pyscf(basis: Basis) -> FloatNxN:
+    return _1e_pyscf(basis, "nuc")
+
+
+def eri_pyscf(basis: Basis) -> FloatNxNxNxN:
+    def func(basis: Basis) -> FloatNxNxNxN:
+        mol = to_pyscf(basis.structure, basis.basis_name)
+        kind = "sph" if basis.spherical else "cart"
+        return jnp.array(mol.intor(f"int2e_{kind}", aosim="s1"), dtype=default_fptype())
+
+    out_spec = jax.ShapeDtypeStruct(
+        shape=4 * (basis.num_orbitals,), dtype=default_fptype()
+    )
+
     return jax.pure_callback(func, out_spec, basis, vmap_method="sequential")
